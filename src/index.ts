4e50b61c0eed1c0ebad1dad92d0897c46daccdad
Chuck Dries <chuck@chuckdries.com>
2022-04-28 21:55:35 -0700

    run prettier

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,18 @@ config();
 
 import { Client, Intents, TextChannel } from "discord.js";
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.once("ready", (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-const NUMS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+const NUMS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
 
 client.on("messageCreate", (message) => {
-  if((message.channel as TextChannel)?.name === 'proofs') {
+  if ((message.channel as TextChannel)?.name === "proofs") {
     for (let i = 0; i < message.attachments.size; i++) {
       message.react(NUMS[i]);
     }
